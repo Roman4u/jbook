43551b6d526837560dc0ac0b2a1c46df9b2ff74c
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2023-09-20 19:31:53 -0700

    fetch service object from esbuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import * as esbuild from "esbuild-wasm";
 
 function App() {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+
+  //this will go into the public directory
+  //find esbuild-wasm and
+  //return a service object that we will use to transpile user code
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    console.log(service);
+  };
+
+  //invoke startService() one time when the component is first rendered to the scree
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = () => {
     console.log(input);
