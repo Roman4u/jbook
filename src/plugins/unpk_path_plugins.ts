8c74efd990705175e10300cc5dbcd63387e1ad91
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2023-10-07 13:03:08 -0700

    refactor resolving various paths in plugin

--- src/plugins/unpk_path_plugins.ts
+++ src/plugins/unpk_path_plugins.ts
@@ -12,24 +12,22 @@ export const unpkgPathPlugin = (userInput: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      //this function retrieves dependency
+      //Handles root entry file of index.js to bundle
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
+
+      //Handles relative paths to modules in UNPKG
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
+        };
+      });
+
+      //Handles main path to main module in UNPKG
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
-
-        if (args.path.includes("./" || args.path.includes("../"))) {
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              "https://unpkg.com" + args.resolveDir + "/"
-            ).href,
-          };
-        }
-
         return { path: `https://unpkg.com/${args.path}`, namespace: "a" };
       });
 
